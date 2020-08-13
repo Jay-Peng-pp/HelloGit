7335151d077d0da089b34853d751f0c04aeb5782
Jay-Peng-pp <18320028151@163.com>
2020-08-14 07:31:57 +0800

    第2次更新后，代码提交

--- HelloGit/src/main/java/cn/jay/test/HelloGit.java
+++ HelloGit/src/main/java/cn/jay/test/HelloGit.java
@@ -4,5 +4,6 @@ package cn.jay.test;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("update 1");
     }
 }

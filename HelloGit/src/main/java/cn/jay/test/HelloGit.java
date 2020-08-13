f192b99c3740fd37c81665c7b7f32d8aa7e3a4f6
Jay-Peng-pp <18320028151@163.com>
2020-08-14 07:54:38 +0800

    分支1第9行添加了代码

--- HelloGit/src/main/java/cn/jay/test/HelloGit.java
+++ HelloGit/src/main/java/cn/jay/test/HelloGit.java
@@ -6,5 +6,6 @@ public class HelloGit {
         System.out.println("Hello Git");
         System.out.println("update 1");
         System.out.println("分支1");
+        System.out.println("分支1第9行代码");
     }
 }
